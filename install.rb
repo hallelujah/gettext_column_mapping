175dea18509f9ef0fc3ecaabe71397aced113159
hallelujah <hery@rails-royce.Org>
2010-06-03 22:38:08 +0200

    Fix installation of column_mapping.yml

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 # Install hook code here
-unless File.exist?(File.join(RAILS_ROOT,'config','column_mapping.yml'))
-  File.copy(File.join(File.dirname(__FILE__),'config','column_mapping.yml'),File.join(Rails.root,'config'))
+unless File.exist?(File.join(RAILS_ROOT,'examples/config','column_mapping.yml'))
+  File.copy(File.join(File.dirname(__FILE__),'examples/config','column_mapping.yml'),File.join(Rails.root,'config'))
 end

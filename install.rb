66b329564f32389099fccc6241fb515678dc99b2
hallelujah <hery@rails-royce.Org>
2010-06-03 22:41:55 +0200

    * Add gettext_db_extract.yml and update install

--- install.rb
+++ install.rb
@@ -1,4 +1,6 @@
 # Install hook code here
-unless File.exist?(File.join(RAILS_ROOT,'examples/config','column_mapping.yml'))
-  File.copy(File.join(File.dirname(__FILE__),'examples/config','column_mapping.yml'),File.join(Rails.root,'config'))
+%{column_mapping.yml gettext_db_extract.yml}.each do |file|
+  unless File.exist?(File.join(RAILS_ROOT,'examples/config',file))
+    File.copy(File.join(File.dirname(__FILE__),'examples/config',file),File.join(Rails.root,'config'))
+  end
 end

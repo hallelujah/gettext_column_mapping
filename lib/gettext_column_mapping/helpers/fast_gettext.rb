789a369329e0b40b557d86596ab2c5652532311a
hallelujah <hery@rails-royce.Org>
2010-06-25 00:00:41 +0200

    Delete unneeded files

--- lib/gettext_column_mapping/helpers/fast_gettext.rb
+++
@@ -1,4 +0,0 @@
-require 'gettext_column_mapping/helpers/fast_gettext/translation'
-require 'gettext_column_mapping/helpers/fast_gettext/storage'
-require 'gettext_column_mapping/helpers/fast_gettext/mo_file'
-require 'gettext_column_mapping/helpers/fast_gettext/translation_repository/base'

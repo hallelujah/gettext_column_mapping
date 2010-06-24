d653e29e1c69aa9feff3b80a6d1c0244c98d69e0
hallelujah <hery@rails-royce.org>
2010-06-24 16:52:23 +0200

    Add a railtie for Rails 3 compatiblity

---
+++ lib/gettext_column_mapping/railtie.rb
@@ -0,0 +1,14 @@
+require 'rails'
+require 'gettext_column_mapping'
+require 'gettext_column_mapping/initializer'
+
+module GettextColumnMapping
+  class Railtie < Rails::Railtie
+
+    rake_tasks do
+      load File.expand_path('../../../tasks/gettext_column_mapping.rake',__FILE__)
+    end
+
+  end
+end
+

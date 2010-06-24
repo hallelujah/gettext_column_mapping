f13be203e1a039bf4141a48de8cd291a78caf873
hallelujah <hery@rails-royce.org>
2010-06-24 12:30:18 +0200

    Update test

--- test/utilisateur_test.rb
+++ test/utilisateur_test.rb
@@ -2,8 +2,11 @@ require 'helper'
 require 'models/utilisateur'
 class UtilisateurTest < Test::Unit::TestCase
   def test_column_names
-    assert_equal 'Gender', Utilisateur.human_attribute_name('sexe')
     assert_equal 'Model|User', Utilisateur.to_s_with_gettext
+    assert_equal 'Model|User|Gender', Utilisateur.gettext_translation_for_attribute_name('sexe')
+    assert_equal 'Model|User|Last name', Utilisateur.gettext_translation_for_attribute_name('nom')
+    assert_equal 'Model|User|First name', Utilisateur.gettext_translation_for_attribute_name('prenom')
+    assert_equal 'Model|User|Age', Utilisateur.gettext_translation_for_attribute_name('age')
   end
 end
 

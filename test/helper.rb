495e46c1a08effcd9a848b553dd46872cc831b6c
hallelujah <hery@rails-royce.org>
2010-06-24 12:04:48 +0200

    Update tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,14 +1,14 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.expand_path('../../lib',__FILE__))
-%w{activesupport activerecord}.each do |p|
-  dir = File.expand_path("../../../rails/#{p}", __FILE__)
+%w{rails/activesupport rails/activerecord fast_gettext gettext_i18n_rails gettext_activerecord}.each do |p|
+  dir = File.expand_path("../../../#{p}", __FILE__)
   if Dir.exists?(dir)
     $LOAD_PATH.unshift(File.join(dir,'lib'))
   end
 end
 require 'rubygems'
-require 'active_record'
-require 'active_support/all'
+require 'gettext_column_mapping'
+require 'gettext_column_mapping/initializer'
 require 'test/unit'
 configuration = {
   'database' => 'gettext_column_mapping',
@@ -28,3 +28,12 @@ ActiveRecord::Migrator.migrate('db/migrate',0)
 # Migrate up
 ActiveRecord::Migrator.migrate('db/migrate',nil)
 $gettext_column_mapping_root = File.dirname(__FILE__)
+
+
+GettextColumnMapping::Initializer.run do |config|
+  config.config_file = File.expand_path("../config/column_mapping.yml", __FILE__)
+end
+FileUtils.mkdir_p(File.join($gettext_column_mapping_root,'locale'))
+FastGettext.add_text_domain 'gettext_column_mapping', :path => File.join($gettext_column_mapping_root,'locale')
+FastGettext.default_available_locales = ['en','fr','es'] #all you want to allow
+FastGettext.default_text_domain =  'gettext_column_mapping'
--- test/utilisateur_test.rb
+++ test/utilisateur_test.rb
@@ -1,3 +1,9 @@
 require 'helper'
 require 'models/utilisateur'
+class UtilisateurTest < Test::Unit::TestCase
+  def test_column_names
+    assert_equal 'Gender', Utilisateur.human_attribute_name('sexe')
+    assert_equal 'Gender', Utilisateur.human_attribute_name('sexe')
+  end
+end
 

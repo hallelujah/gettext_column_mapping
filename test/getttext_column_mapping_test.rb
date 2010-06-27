2f5a118280d25fd95110e8d0c13930231d68fe53
hallelujah <hery@rails-royce.Org>
2010-06-27 21:54:26 +0200

    Add some more tests

--- test/getttext_column_mapping_test.rb
+++ test/getttext_column_mapping_test.rb
@@ -3,9 +3,17 @@ class GettextColumnMappingTest < Test::Unit::TestCase
 
   def test_config
     assert_instance_of ActiveSupport::OrderedOptions, GettextColumnMapping.config
-    GettextColumnMapping.config.mapping_file = File.join($gettext_column_mapping_root, 'config/column_mapping.yml')
-    assert_not_nil GettextColumnMapping.config.mapping_file
     assert_instance_of GettextColumnMapping::Mapper, GettextColumnMapping.mapper
+    assert_equal :yaml, GettextColumnMapping.config.config_parser
+    assert_equal :gettext_i18n_rails, GettextColumnMapping.config.backend
+    assert_not_nil GettextColumnMapping.config.backend_class
+    assert_equal "Model", GettextColumnMapping.config.model_prefix
+    assert_equal "Data", GettextColumnMapping.config.data_prefix
+
+    GettextColumnMapping.config.config_file = File.join($gettext_column_mapping_root, 'config/column_mapping.yml')
+    assert_not_nil GettextColumnMapping.config.config_file
+
+    GettextColumnMapping::Initializer.run
   end
 
 end

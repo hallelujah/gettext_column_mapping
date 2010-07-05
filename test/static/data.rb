5a2d087e12dc8c5c1aae1b8cf459a2d8babd9f3a
hallelujah <hery@rails-royce.org>
2010-07-05 16:59:49 +0200

    Fix po/pot files + static

--- test/static/data.rb
+++ test/static/data.rb
@@ -1,2 +1,7 @@
 # coding: utf-8
 #DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!
+s_('Model|User')
+s_('Model|User|First name')
+s_('Model|User|Last name')
+s_('Model|User|Gender')
+#DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!

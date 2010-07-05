c379c5336f5ccc46aa9f085b817ae59e958e8d9c
hallelujah <hery@rails-royce.org>
2010-07-05 12:25:28 +0200

    Update ignored files

---
+++ test/locale/data.rb
@@ -0,0 +1,6 @@
+#DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!
+s_('Model|User')
+s_('Model|User|First name')
+s_('Model|User|Last name')
+s_('Model|User|Gender')
+#DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!

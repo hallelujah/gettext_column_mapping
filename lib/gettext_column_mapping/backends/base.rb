d0cab2e0bf467299c8cd0396fd1337e9863a0844
hallelujah <hery@rails-royce.org>
2010-07-05 11:54:13 +0200

    Fix backends/base API methods

--- lib/gettext_column_mapping/backends/base.rb
+++ lib/gettext_column_mapping/backends/base.rb
@@ -1,4 +1,8 @@
 module GettextColumnMapping
+
+  mattr_accessor :activerecord_subclasses
+  self.activerecord_subclasses = []
+
   module Backends
     module Base
 
@@ -6,15 +10,37 @@ module GettextColumnMapping
         base.class_eval do
           class << self
 
+            def inherited_with_translation(subclass)
+              inherited_without_translation(subclass)
+#             subclass.attr_accessor_with_column_translation(subclass.translation_column_attributes,subclass.translation_parent_attributes)
+              GettextColumnMapping.activerecord_subclasses << subclass
+            end
+            alias_method_chain :inherited, :translation
+
+            def untranslate_all? #_with_untranslatable_columns?
+              ! translate_class_name?
+            end
+
+            # Checks if the column has to be translated
+            def untranslate?(columnname)
+              ! translate_key?(columnname)
+            end
+
+            def to_mapping
+              Gettext.mapper.class_mapping(self)
+            rescue
+              name.underscore
+            end
+
             def gettext_translation_for_attribute_name(attribute_key_name)
               GettextColumnMapping.mapper.column_translation_for_attribute(self,attribute_key_name.to_s)
             end
 
-            def has_translation_key?(attr_name)
+            def translate_key?(attr_name)
               GettextColumnMapping.mapper.translate_key?(self,attr_name)
             end
 
-            def has_translation_class_name?
+            def translate_class_name?
               GettextColumnMapping.mapper.translate_class_name?(self)
             end
 

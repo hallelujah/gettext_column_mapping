1c151656e7935fc9f1bc1e54c2319a7502fc0755
hallelujah <hery@rails-royce.org>
2010-07-06 18:32:23 +0200

    Remove un-necessary code in init

--- init.rb
+++ init.rb
@@ -1,10 +1,2 @@
 # coding: utf-8
-# Include hook code here
-# HOOK inspired by gettext_i18n_rails
-begin
-  require 'config/initializers/session_store'
-rescue LoadError
-  # weird bug, when run with rake rails reports error that session
-  # store is not configured, this fixes it somewhat...
-end
 require 'gettext_column_mapping'

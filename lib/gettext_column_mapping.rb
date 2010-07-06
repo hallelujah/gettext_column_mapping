8345563f2fd593ca21bf03324654178384651b57
hallelujah <hery@rails-royce.org>
2010-07-06 12:24:20 +0200

    Revert Proc, make config options mandatory and automatic bootup
    We compels developpers to set up :
      * config.gettext_column_mapping.config_file
    
    In Rails 3, the railtie boots up automatically

--- lib/gettext_column_mapping.rb
+++ lib/gettext_column_mapping.rb
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 # Check the version of active_support to requiring all libs
 require 'active_support/version'
 if Gem::Version.new(ActiveSupport::VERSION::STRING) > Gem::Version.new("2")
@@ -19,21 +18,21 @@ module GettextColumnMapping
   mattr_accessor :config
   mattr_reader :mapper
   @@mapper = GettextColumnMapping::Mapper.new
-  
+
   # Configuration default
 
   self.config = ActiveSupport::OrderedOptions.new
-  self.config.config_file = defined?(Rails) ? File.join(Rails.root,'config','column_mapping.yml') : nil
+  self.config.config_file =  nil
   self.config.config_parser = :yaml
   self.config.backend = :gettext_i18n_rails
   self.config.backend_class = nil
   self.config.model_prefix = "Model"
   self.config.data_prefix = "Data"
   self.config.charset = :utf8
-  
+
   # Additional plugins
   self.config.use_parent_level = false
-  self.config.parent_level_file = defined?(Rails) ? File.join(Rails.root,'config','parent_level_column_mapping.yml') : nil
+  self.config.parent_level_file = nil
   self.config.parent_level_parser = :yaml
 
   def self.charset
--- lib/gettext_column_mapping/railtie.rb
+++ lib/gettext_column_mapping/railtie.rb
@@ -6,8 +6,14 @@ require 'gettext_column_mapping/initializer'
 module GettextColumnMapping
   class Railtie < Rails::Railtie
 
+    config.gettext_column_mapping = GettextColumnMapping.config
+
+    initializer "gettext_column_mapping.after_initialize" do |app|
+      GettextColumnMapping::Initializer.run(app.config.gettext_column_mapping)
+    end
+
     rake_tasks do
-      load File.expand_path('../../../tasks/gettext_column_mapping.rake',__FILE__)
+      load File.expand_path('../tasks.rb',__FILE__)
     end
 
   end

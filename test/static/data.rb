fa9979b8e0f2898723c8b862c50d372b3390e110
hallelujah <hery@rails-royce.org>
2010-07-06 10:28:29 +0200

    Fix parent_level_column_mapping.yml config
    columns must be an Array of fields

--- test/static/data.rb
+++ test/static/data.rb
@@ -8,4 +8,8 @@ s_('Model|User')
 s_('Model|User|First name')
 s_('Model|User|Last name')
 s_('Model|User|Gender')
+s_('Data|Model|Category|Sport|Label|Football')
+s_('Data|Model|Category|Sport|Label|World Cup')
+s_('Data|Model|Category|Event|Label|Football')
+s_('Data|Model|Category|Event|Label|World Cup')
 #DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!
